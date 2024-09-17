c2343aaf1ac258f8dea9cebfee1c9cd3579cc4c4
Lekanlekzy <dauda629@gmail.com>
2024-09-16 22:38:22 -0600

    npm

--- Lekan/src/App.jsx
+++ Lekan/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './Navbar.jsx'
 
 function App() {
 
 
   return (
-
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
