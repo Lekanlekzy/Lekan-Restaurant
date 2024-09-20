9f110d50b48526f0f7e60b31a324fb7a7cbb623d
Lekanlekzy <dauda629@gmail.com>
2024-09-19 19:51:40 -0600

    Folder created

--- Lekan/src/App.jsx
+++ Lekan/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from '../src/container/Navbar'
+import Navbar from './container/Navbar/Navbar'
 
 function App() {
 
