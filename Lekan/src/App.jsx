edbbaae24f875e2f6be564cf800be557d4fbab95
Lekanlekzy <dauda629@gmail.com>
2024-09-25 23:07:44 -0600

    app +container

--- Lekan/src/App.jsx
+++ Lekan/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './container/Navbar/Navbar'
+import {Navbar, Header} from './container'
 
 function App() {
 
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      {/* <Header />
-      <AboutUs />
+       <Header />
+      {/*<AboutUs />
       <SpecialMenu />
       <Chef />
       <Intro />
