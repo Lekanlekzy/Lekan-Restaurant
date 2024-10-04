378f2b18bc7fe39d7a5e8c1764ba032408bc7235
Lekanlekzy <dauda629@gmail.com>
2024-10-03 22:30:34 -0600

    iu

--- Lekan/src/App.jsx
+++ Lekan/src/App.jsx
@@ -2,24 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import {AboutUs, Intro, Laurels, SpecialMenu, Chef, Footer, Gallery, FindUs, Navbar, Header} from './container/index'
 // import { Navbar } from './container'
+// import {AboutUs, Intro, Laurels, SpecialMenu, Chef, Footer, Gallery, FindUs, Navbar, Header} from './container/index'
+// import { Header } from './container/index'
 function App() {
 
 
   return (
     <div>
       <h1>Check</h1>
-      {/* <Navbar />
-       <Header />
-      <AboutUs />
+      <Navbar ></Navbar>
+       {/* <Navbar /> */}
+       {/* <Header /> */}
+     {/* <AboutUs />
       <SpecialMenu />
       <Chef />
       <Intro />
       <Laurels />
       <Gallery />
-      <FindUs />
-      <Footer /> */}
+      <FindUs />*/}
+      {/* <Footer />  */}
 
     </div>
   )
